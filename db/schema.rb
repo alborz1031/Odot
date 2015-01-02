53dd445cfb1ff8f595f57ad93728bc68acc86b0a
alborz1031 <alborz1031@gmail.com>
2015-01-02 13:55:04 -0500

    Added bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206195317) do
+ActiveRecord::Schema.define(version: 20141219232801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
